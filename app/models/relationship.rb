eabca94b3f7cd1ca6ee159f6817178331e3dffde
宮嶋 <takaki.mz@icloud.com>
2015-12-14 16:37:03 +0000

    Finish user follow / unfollow actions

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,5 @@
 class Relationship < ActiveRecord::Base
-　belongs_to :follower, class_name: "User"
+  belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-　has_many :followed_users, through: :relationships, source: :followed
+  has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
                                    dependent:   :destroy
-  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :followers, through: :reverse_relationships,source: :follower
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
