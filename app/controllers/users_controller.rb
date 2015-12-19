11968ef194375ce1dc7013fb2113640b8fce72df
宮嶋 <takaki.mz@icloud.com>
2015-12-19 08:33:45 +0000

    Finish user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,21 @@ class UsersController < ApplicationController
     end
   end
   
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   private
     
     def admin_user
